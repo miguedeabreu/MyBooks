3e08acee125b9fd77d70db94833304f888015e77
miguedeabreu <miguedeabreu@gmail.com>
2022-03-15 15:21:36 +0100

    retos del dia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "registro", component: RegistroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
